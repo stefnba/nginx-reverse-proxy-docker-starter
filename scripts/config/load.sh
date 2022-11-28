8a9c38984f8a83a30601ece4a705657aab423d03
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-11-28 10:10:25 +0100

    fix: bug fixed to make everything work

--- scripts/config/load.sh
+++ scripts/config/load.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-template_path="../../template/template.conf"
+##
+# Loads config files in /sites dir into /config/sites which are then loaded into Nginx container.
+# Script should be run every time config files changes
+##
+
+template_path="../../templates"
+sites_path="../../sites"
+output_path="../../config/sites"
 
 cd $(dirname $0)
+
+mkdir -p $output_path
+
+for site in "$sites_path"/*
+do
+    . $site # open file
+    main_domain=${domain%%' '*} # get first element of domain list
+
+    export main_domain=$main_domain
+    export domain=$domain
+    export service=$service
+
+    name="$(basename $site)"
+
+    # echo "$site ${$name%.*}"
+
+    if [ "$https" == 'true' ]; 
+        then
+            cat "$template_path/https.conf" | envsubst > "$output_path/$(basename $site).nginx"
+        else
+            cat "$template_path/http.conf" | envsubst > "$output_path/$(basename $site).nginx"
+    fi
+done
 
 
--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -5,22 +5,37 @@
 ##
 
 files_path="./files.conf"
+env_file="../../.env"
 
 cd $(dirname $0)
 
-env_file="../../.env"
-# files=("../..//./docker-compose.yml" "../..//./config/*" "../..//./servers/*" "../..//./scripts/*")
-files=. $files_path
+files=(
+    "../..//./docker-compose.yml"
+    "../..//./config/nginx.conf"
+    "../..//./config/proxy_params.conf"
+    "../..//./scripts/ssl/*"
+    "../..//./scripts/config/*"
+    "../..//./templates/*"
+    "../..//./sites/*"
+)
+
+# file_list=()
+
+# while IFS= read -r line; do
+#     echo $line
+#   file_list+=("$line")
+# done < $files_path
+
+# echo "${file_list[@]}"
 
 # Read .env file
 source $env_file
 
-echo $REMOTE_DIR
+# echo "${files[@]}"
 
 # Sync files
 for file in "${files[@]}"
 do
+    # echo $file
     rsync $file "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" -v -R -r
-    echo "__ DONE __"
-    echo ""
 done
